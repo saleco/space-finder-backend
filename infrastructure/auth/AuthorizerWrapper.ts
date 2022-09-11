7b0de55a04c2eb096de574f8012dc9a118d89179
saleco <salloszrajbman@hotmail.com>
2022-09-11 12:08:40 +0100

    Identity Pools

--- infrastructure/auth/AuthorizerWrapper.ts
+++ infrastructure/auth/AuthorizerWrapper.ts
@@ -2,6 +2,7 @@ import { CfnOutput } from "aws-cdk-lib";
 import { CognitoUserPoolsAuthorizer, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { UserPool, UserPoolClient, CfnUserPoolGroup } from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
+import { IdentityPoolWrapper } from "./IdentityPoolWrapper";
 
 export class AuthorizerWrapper {
 
@@ -11,6 +12,7 @@ export class AuthorizerWrapper {
     private userPool: UserPool;
     private userPoolClient: UserPoolClient;
     public authorizer: CognitoUserPoolsAuthorizer;
+    private identityPoolWrapper: IdentityPoolWrapper;
 
     constructor(scope: Construct, api: RestApi){
         this.scope = scope;
@@ -22,6 +24,7 @@ export class AuthorizerWrapper {
         this.createUserPool();
         this.addUserPoolClient();
         this.createAuthorizer();
+        this.initializeIdentityPoolWrapper();
         this.createAdminsGroup();
     }
 
@@ -67,10 +70,19 @@ export class AuthorizerWrapper {
         this.authorizer._attachToApi(this.api);
     }
 
+    private initializeIdentityPoolWrapper() {
+        this.identityPoolWrapper = new IdentityPoolWrapper(
+            this.scope, 
+            this.userPool, 
+            this.userPoolClient
+        )
+    }
+
     private createAdminsGroup() {
         new CfnUserPoolGroup(this.scope, 'admins', {
             groupName: 'admins',
-            userPoolId: this.userPool.userPoolId
+            userPoolId: this.userPool.userPoolId,
+            roleArn: this.identityPoolWrapper.adminRole.roleArn
         })
     }
 }
--- test/Auth/auth.test.ts
+++ test/Auth/auth.test.ts
@@ -1,5 +1,30 @@
 import { AuthService } from "./AuthService";
 import { config } from "./config";
+import * as AWS from 'aws-sdk'
+import { S3 } from "aws-sdk";
 
-const authService = new AuthService();
-const user = authService.login(config.TEST_USER_NAME, config.TEST_USER_PASSWORD);
+AWS.config.region = config.REGION;
+
+async function getBuckets(){
+    let buckets;
+    try {
+        buckets = await new S3().listBuckets().promise();
+    } catch (error) {
+        buckets = undefined;
+    }
+    return buckets;
+}
+
+async function callStuff() {
+    const authService = new AuthService();
+    const user = await authService.login(config.TEST_USER_NAME, config.TEST_USER_PASSWORD);
+    await authService.getAWSTemporaryCreds(user);
+    const someCreds = AWS.config.credentials;
+    const buckets = await getBuckets();
+    const a = 5
+
+}
+
+
+
+callStuff();
